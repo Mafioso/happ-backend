82f6cc863183439b9eb0629bd4442a36181cd207
Askhat Omarov <askhat.omarov91@gmail.com>
2016-08-16 16:33:34 +0600

    datefield can receive also date object; timefield can receive also time object

--- happ/tests/test_unit.py
+++ happ/tests/test_unit.py
@@ -37,11 +37,18 @@ class EventTests(TestCase):
             event.save()
 
         self.assertIsInstance(Event.objects.first().start_date, datetime.date)
+        
         self.assertEqual(Event.objects.filter(start_date=datetime.datetime(2016, 10, 2)).count(), 1)
         self.assertEqual(Event.objects.filter(start_date__gt=datetime.datetime(2016, 10, 2)).count(), 3)
         self.assertEqual(Event.objects.filter(start_date__gte=datetime.datetime(2016, 10, 2)).count(), 4)
         self.assertEqual(Event.objects.filter(start_date__lt=datetime.datetime(2016, 10, 2)).count(), 1)
         self.assertEqual(Event.objects.filter(start_date__lte=datetime.datetime(2016, 10, 2)).count(), 2)
+
+        self.assertEqual(Event.objects.filter(start_date=datetime.date(2016, 10, 2)).count(), 1)
+        self.assertEqual(Event.objects.filter(start_date__gt=datetime.date(2016, 10, 2)).count(), 3)
+        self.assertEqual(Event.objects.filter(start_date__gte=datetime.date(2016, 10, 2)).count(), 4)
+        self.assertEqual(Event.objects.filter(start_date__lt=datetime.date(2016, 10, 2)).count(), 1)
+        self.assertEqual(Event.objects.filter(start_date__lte=datetime.date(2016, 10, 2)).count(), 2)
 
     def test_event_time_field(self):
         """
@@ -54,8 +61,15 @@ class EventTests(TestCase):
             event.save()
 
         self.assertIsInstance(Event.objects.first().start_time, datetime.time)
+        
         self.assertEqual(Event.objects.filter(start_time=datetime.datetime(2016, 10, 1, 10, 2, 30)).count(), 1)
         self.assertEqual(Event.objects.filter(start_time__gt=datetime.datetime(2016, 10, 1, 10, 2, 30)).count(), 3)
         self.assertEqual(Event.objects.filter(start_time__gte=datetime.datetime(2016, 10, 1, 10, 2, 30)).count(), 4)
         self.assertEqual(Event.objects.filter(start_time__lt=datetime.datetime(2016, 10, 1, 10, 2, 30)).count(), 1)
         self.assertEqual(Event.objects.filter(start_time__lte=datetime.datetime(2016, 10, 1, 10, 2, 30)).count(), 2)
+        
+        self.assertEqual(Event.objects.filter(start_time=datetime.time(10, 2, 30)).count(), 1)
+        self.assertEqual(Event.objects.filter(start_time__gt=datetime.time(10, 2, 30)).count(), 3)
+        self.assertEqual(Event.objects.filter(start_time__gte=datetime.time(10, 2, 30)).count(), 4)
+        self.assertEqual(Event.objects.filter(start_time__lt=datetime.time(10, 2, 30)).count(), 1)
+        self.assertEqual(Event.objects.filter(start_time__lte=datetime.time(10, 2, 30)).count(), 2)
