0537572282be40d1a00459e51107e2c451d2b105
Askhat Omarov <askhat.omarov91@gmail.com>
2016-08-22 15:06:46 +0600

    tests for currencies

--- happ/tests/test_api.py
+++ happ/tests/test_api.py
@@ -30,3 +30,28 @@ class CitiesTests(APISimpleTestCase):
         self.client.credentials(HTTP_AUTHORIZATION='{} {}'.format(api_settings.JWT_AUTH_HEADER_PREFIX, response.data['token']))
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class CurrenciesTests(APISimpleTestCase):
+
+    def test_get_without_auth(self):
+        url = reverse('currencies-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_with_auth(self):
+        u = UserFactory()
+        u.set_password('123')
+        u.save()
+
+        auth_url = reverse('login')
+        data = {
+            'username': u.username,
+            'password': '123'
+        }
+        response = self.client.post(auth_url, data=data, format='json')
+        
+        url = reverse('currencies-list')
+        self.client.credentials(HTTP_AUTHORIZATION='{} {}'.format(api_settings.JWT_AUTH_HEADER_PREFIX, response.data['token']))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
