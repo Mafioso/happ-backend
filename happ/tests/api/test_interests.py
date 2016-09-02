e9a53b3899127a0e565bbe516f4cbde9579d1fbe
Askhat Omarov <askhat.omarov91@gmail.com>
2016-09-02 12:26:26 +0600

    more tests

--- happ/tests/api/test_interests.py
+++ happ/tests/api/test_interests.py
@@ -21,6 +21,33 @@ from .. import *
 
 
 class EventTests(APISimpleTestCase):
+    def test_get_without_auth(self):
+        """
+        Resourse is not available without authentication
+        """
+        url = prepare_url('interests-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_with_auth(self):
+        """
+        Resourse is available with authentication only
+        """
+        u = UserFactory()
+        u.set_password('123')
+        u.save()
+
+        auth_url = prepare_url('login')
+        data = {
+            'username': u.username,
+            'password': '123'
+        }
+        response = self.client.post(auth_url, data=data, format='json')
+
+        url = prepare_url('interests-list')
+        self.client.credentials(HTTP_AUTHORIZATION='{} {}'.format(api_settings.JWT_AUTH_HEADER_PREFIX, response.data['token']))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_event(self):
         """
