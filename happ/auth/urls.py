1de3a4d5df130e16274a2514c3e6e1427cc53d1c
Askhat Omarov <askhat.omarov91@gmail.com>
2016-09-05 12:58:00 +0600

    refresh auth token api

--- happ/auth/urls.py
+++ happ/auth/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from .views import UserRegister, PasswordChange, PasswordReset, PasswordResetConfirm
 
 urlpatterns = [
     url(r'login/$', obtain_jwt_token, name='login'),
+    url(r'refresh/$', refresh_jwt_token, name='refresh'),
     url(r'register/$', UserRegister.as_view(), name='register'),
     url(r'password/change/$', PasswordChange.as_view(), name='password-change'),
     url(r'password/reset/$', PasswordReset.as_view(), name='password-reset'),
--- happ/settings/common.py
+++ happ/settings/common.py
@@ -145,6 +145,7 @@ REST_FRAMEWORK = {
 JWT_AUTH = {
     'JWT_PAYLOAD_HANDLER': 'happ.utils.jwt_payload_handler',
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=365),
+    'JWT_ALLOW_REFRESH': True,
 }
 
 AUTHENTICATION_BACKENDS = ('happ.auth.backends.HappAuthBackend', )
