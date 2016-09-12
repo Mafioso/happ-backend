4441a2653562d0db40d97ee23172cccb8f3c9deb
Askhat Omarov <askhat.omarov91@gmail.com>
2016-09-12 17:08:46 +0600

    return coutries as results

--- happ/api/countries.py
+++ happ/api/countries.py
@@ -1,4 +1,5 @@
 from rest_framework import mixins
+from rest_framework.response import Response
 from rest_framework_mongoengine import viewsets
 
 from ..models import Country
@@ -9,3 +10,9 @@ class CountryViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
     pagination_class = None
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'results': serializer.data})
