d635665d333836dc38c45e4146c66c29f0b30409
Askhat Omarov <askhat.omarov91@gmail.com>
2016-09-13 12:40:10 +0600

    added cookie based JWT authentication

--- happ/settings/common.py
+++ happ/settings/common.py
@@ -147,6 +147,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'happ.auth.authentication.CookieJSONWebTokenAuthentication',
     ),
 }
 
