0da084d6a9503df0fb3058f9f4ccdec30dac306b
Askhat Omarov <askhat.omarov91@gmail.com>
2016-09-24 01:25:01 +0600

    more elegant way using decorators

--- happ/api/admin/interests.py
+++ happ/api/admin/interests.py
@@ -4,7 +4,7 @@ from rest_framework_mongoengine import viewsets
 
 from mongoextensions import filters
 from happ.models import Interest
-from happ.decorators import patch_serializer_class
+from happ.decorators import patch_serializer_class, patch_queryset
 from happ.serializers import InterestSerializer, InterestParentSerializer, InterestChildSerializer
 
 
@@ -15,27 +15,17 @@ class InterestViewSet(viewsets.ModelViewSet):
     search_fields = ('title', )
 
     @list_route(methods=['get'], url_path='categories')
+    @patch_queryset(lambda x: x.filter(parent=None))
     @patch_serializer_class(InterestParentSerializer)
     def categories(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset().filter(parent=None))
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response({'results': serializer.data})
+        response = super(InterestViewSet, self).list(request, *args, **kwargs)
+        # response.template_name =
+        return response
 
     @list_route(methods=['get'], url_path='children')
+    @patch_queryset(lambda x: x.filter(parent__ne=None))
     @patch_serializer_class(InterestChildSerializer)
     def children(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset().filter(parent__ne=None))
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response({'results': serializer.data})
+        response = super(InterestViewSet, self).list(request, *args, **kwargs)
+        # response.template_name =
+        return response
--- happ/decorators.py
+++ happ/decorators.py
@@ -9,3 +9,16 @@ def patch_serializer_class(serializer_class):
             return rv
         return wrapper
     return decorator
+
+def patch_queryset(funct):
+
+    def decorator(fn):
+        def wrapper(self, request, *a, **kw):
+            prev_queryset = self.queryset
+            self.queryset = funct(prev_queryset)
+            rv = fn(self, request, *a, **kw)
+            self.queryset = prev_queryset
+            return rv
+        return wrapper
+    return decorator
+
