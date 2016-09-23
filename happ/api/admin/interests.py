35c19c00bac4d16145af1ae626a03136212e4237
Askhat Omarov <askhat.omarov91@gmail.com>
2016-09-24 01:07:49 +0600

    admin interests categories

--- happ/api/admin/interests.py
+++ happ/api/admin/interests.py
@@ -1,8 +1,11 @@
+from rest_framework.response import Response
+from rest_framework.decorators import list_route
 from rest_framework_mongoengine import viewsets
 
 from mongoextensions import filters
 from happ.models import Interest
-from happ.serializers import InterestSerializer
+from happ.decorators import patch_serializer_class
+from happ.serializers import InterestSerializer, InterestParentSerializer
 
 
 class InterestViewSet(viewsets.ModelViewSet):
@@ -10,3 +13,16 @@ class InterestViewSet(viewsets.ModelViewSet):
     queryset = Interest.objects.all()
     filter_backends = (filters.MongoSearchFilter, )
     search_fields = ('title', )
+
+    @list_route(methods=['get'], url_path='categories')
+    @patch_serializer_class(InterestParentSerializer)
+    def categories(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset().filter(parent=None))
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'results': serializer.data})
