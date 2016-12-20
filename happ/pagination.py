d05f430a6c514cda74f14aad5a44f5afec4c098d
Askhat Omarov <askhat.omarov91@gmail.com>
2016-12-20 19:43:16 +0600

    fixed pagination comment

--- happ/pagination.py
+++ happ/pagination.py
@@ -6,19 +6,10 @@ from rest_framework.response import Response
 
 class SolidPagination(BasePagination):
     """
-    A simple page number based style that supports page numbers as
-    query parameters. For example:
-
+    No pagination actually. It returns whole data, but wraps it as object with `results` key.
     """
-    # The default page size.
-    # Defaults to `None`, meaning pagination is disabled.
 
     def paginate_queryset(self, queryset, request, view=None):
-        """
-        Paginate a queryset if required, either returning a
-        page object, or `None` if pagination is not configured for this view.
-        """
-
         self.request = request
         return list(queryset)
 
