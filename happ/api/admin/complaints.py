15aa11bdb9d84e99e0c6065eeb82f8fa9b77bac7
Askhat Omarov <askhat.omarov91@gmail.com>
2016-12-21 10:16:55 +0600

    reply complaints api endpoint and tests

--- happ/api/admin/complaints.py
+++ happ/api/admin/complaints.py
@@ -1,4 +1,6 @@
 from rest_framework import mixins
+from rest_framework.response import Response
+from rest_framework.decorators import detail_route
 from rest_framework_mongoengine import viewsets
 
 from happ.models import Complaint
@@ -15,3 +17,12 @@ class ComplaintViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
         response = super(ComplaintViewSet, self).list(request, *args, **kwargs)
         response.template_name = 'admin/complaints/list.html'
         return response
+
+    @detail_route(methods=['post'], url_path='reply')
+    def reply(self, request, *args, **kwargs):
+        instance = self.get_object()
+        answer = request.data.get('answer', '')
+        executor = request.user
+        instance.reply(answer=answer, executor=executor)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
---
+++ happ/tests/api/admin/test_complaints.py
@@ -0,0 +1,88 @@
+from rest_framework import status
+from rest_framework.test import APISimpleTestCase
+from rest_framework_jwt.settings import api_settings
+
+from happ.models import User, Complaint
+from happ.factories import (
+    UserFactory,
+    ComplaintFactory,
+)
+from happ.tests import *
+
+
+class Tests(APISimpleTestCase):
+
+    def test_get_without_auth(self):
+        """
+        Resourse is not available without authentication
+        """
+        url = prepare_url('admin-complaints-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_with_auth_not_staff(self):
+        """
+        Resourse is not available for non-staff users
+        """
+        u = UserFactory()
+        u.set_password('123')
+        u.save()
+
+        auth_url = prepare_url('login')
+        data = {
+            'username': u.username,
+            'password': '123'
+        }
+        response = self.client.post(auth_url, data=data, format='json')
+        token = response.data['token']
+
+        url = prepare_url('admin-complaints-list')
+        self.client.credentials(HTTP_AUTHORIZATION='{} {}'.format(api_settings.JWT_AUTH_HEADER_PREFIX, token))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_with_auth(self):
+        """
+        Resourse is available with authentication only and for staff
+        """
+        u = UserFactory(role=User.MODERATOR)
+        u.set_password('123')
+        u.save()
+
+        auth_url = prepare_url('login')
+        data = {
+            'username': u.username,
+            'password': '123'
+        }
+        response = self.client.post(auth_url, data=data, format='json')
+        token = response.data['token']
+
+        url = prepare_url('admin-complaints-list')
+        self.client.credentials(HTTP_AUTHORIZATION='{} {}'.format(api_settings.JWT_AUTH_HEADER_PREFIX, token))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_reply(self):
+        u = UserFactory(role=MODERATOR)
+        u.set_password('123')
+        u.save()
+
+        c = ComplaintFactory()
+
+        auth_url = prepare_url('login')
+        data = {
+            'username': u.username,
+            'password': '123'
+        }
+        response = self.client.post(auth_url, data=data, format='json')
+        token = response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='{} {}'.format(api_settings.JWT_AUTH_HEADER_PREFIX, token))
+
+        data = {
+            'answer': 'Considered',
+        }
+
+        url = prepare_url('admin-complaints-reply', kwargs={'id': str(c.id)})
+        response = self.client.post(url, data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
