c7e57f8ecc2654e4035ac4f36b0f82743a82fa64
Askhat Omarov <askhat.omarov91@gmail.com>
2017-01-06 15:58:25 +0600

    prod g key

--- happ/settings/prod.py
+++ happ/settings/prod.py
@@ -5,4 +5,5 @@ MONGODB_NAME = 'happ_prod'
 DEBUG = False
 STATIC_ROOT = os.path.join('/', 'static')
 
+GOOGLE_BROWSER_KEY = 'AIzaSyDi4DJypjXFrG68LVJU6EGdK-rHryYVyRw'
 GOOGLE_API_KEY = 'AIzaSyCD8N02qqKXT5QHEWt8jk28F2AJ8kKIsrA'
