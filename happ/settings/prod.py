c2792d955f03723c696d349130278d627c9028a9
xepa4ep <mailubai@gmail.com>
2017-01-18 11:10:35 +0600

    new mailgun account

--- happ/settings/prod.py
+++ happ/settings/prod.py
@@ -7,3 +7,8 @@ STATIC_ROOT = os.path.join('/', 'static')
 
 GOOGLE_BROWSER_KEY = 'AIzaSyDi4DJypjXFrG68LVJU6EGdK-rHryYVyRw'
 GOOGLE_API_KEY = 'AIzaSyCD8N02qqKXT5QHEWt8jk28F2AJ8kKIsrA'
+ANYMAIL = {
+    "MAILGUN_API_KEY": "key-4954691d2cfc091f07b80b0100806dc7",
+    "MAILGUN_SENDER_DOMAIN": 'notifications.happapp.info',
+}
+DEFAULT_FROM_EMAIL = 'no-reply@notifications.happapp.info'
