7062111020e751a49c21314e42c5fa9d265814fd
Askhat Omarov <askhat.omarov91@gmail.com>
2017-01-19 16:08:54 +0600

    log filter: date_created

--- happ/filters.py
+++ happ/filters.py
@@ -31,8 +31,8 @@ class EventFilter(filtersets.ModelFilterset):
 
 
 class LogEntryFilter(filtersets.ModelFilterset):
-    start_date = filters.DateTimeFilter(name='date_created', lookup='gte')
-    end_date = filters.DateTimeFilter(name='date_created', lookup='lte')
+    start_date = filters.DateTimeFilter(source='date_created', lookup='gte')
+    end_date = filters.DateTimeFilter(source='date_created', lookup='lte')
 
     class Meta:
         model = LogEntry
